549b5028cc374277ded7b1f02e42da324e7358e0
abdulloh-uzb <usmonovabdulloh87@gmail.com>
2025-07-15 10:32:59 +0500

    feat: protect routes using Spatie role middleware

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,15 @@ use Illuminate\Support\Facades\Route;
 Route::redirect('/', '/home');
 Route::get("home", [TeacherController::class, 'home'])->name('teachers.home');
 
-Route::get("groups", [GroupController::class, 'index'])->name('groups.index');
-Route::get("groups/{group}", [GroupController::class, 'show'])->name('groups.show');
+Route::middleware(['auth', 'role:teacher'])->group(function () {
+    Route::get("groups", [GroupController::class, 'index'])->name('groups.index');
+    Route::get("groups/{group}", [GroupController::class, 'show'])->name('groups.show');
 
-Route::get('/sessions/{session}/attendance', [SessionController::class, 'show'])->name('sessions.attendance');
-Route::post('sessions/attendance', [SessionController::class, 'setStatus'])->name('sessions.attendance.store');
+    Route::get('/sessions/{session}/attendance', [SessionController::class, 'show'])->name('sessions.attendance');
+    Route::post('sessions/attendance', [SessionController::class, 'setStatus'])->name('sessions.attendance.store');
 
+    Route::get("students", [StudentController::class, 'index'])->name('students.index');
 
-Route::get("students", [StudentController::class, 'index'])->name('students.index');
+});
+
 Route::post('telegram/webhook', [TelegramController::class, "handle"]);
